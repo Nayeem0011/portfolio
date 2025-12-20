47874c51a77ac2bf624fb86dec7e475ab3d92ed7
Nayeem0011 <sknayeemislam384@gmail.com>
2025-12-21 04:34:34 +0600

    first commit

---
+++ app/component/Body.js
@@ -0,0 +1,43 @@
+'use client';
+
+import HeroPage from './HeroPage';
+import NavBar from './NavBar';;
+import Pageone from './Pageone';
+import Pagetwo from './Pagetwo';
+import Pagethree from './Pagethree';
+import Pagefore from './Pagefore';
+import Pagefive from './Pagefive';
+import Footer from './Footer';
+
+const Body = () => {
+    return (
+        <div className="bg-background-light dark:bg-background-dark font-display text-gray-800 dark:text-gray-200">
+            <div className="min-h-screen flex flex-col">
+                <div className="px-4 sm:px-6 lg:px-8 sticky top-0 z-50">
+                    <NavBar />
+                </div>
+                <div id="home" className="px-4 sm:px-6 lg:px-8">
+                    <HeroPage />
+                </div>
+                <Pageone />
+                <div className="px-4 sm:px-6 lg:px-8">
+                    <div id="about">
+                        <Pagetwo />
+                    </div>
+                    <div id="skills">
+                        <Pagethree />
+                    </div>
+                    <div id="projects">
+                        <Pagefore />
+                    </div>
+                    <div id="contact">
+                        <Pagefive />
+                    </div>
+                </div>
+                <Footer />
+            </div>
+        </div>
+    );
+};
+
+export default Body;
